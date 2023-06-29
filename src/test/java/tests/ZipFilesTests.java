968b418081ed9cc53dcbcb693168f18b0d51dffa
Evgenii Klimashin <evgeniiklimashin99@gmail.com>
2023-06-29 23:18:51 +0300

    Add null check

--- src/test/java/tests/ZipFilesTests.java
+++ src/test/java/tests/ZipFilesTests.java
@@ -8,51 +8,67 @@ import model.GlossaryModel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class ZipFilesTests {
 
+    @Test
+    void testPdf() throws Exception {
+        try (ZipFile zipFile = new ZipFile(new File("src/test/resources/zipFiles.zip"))) {
+            ZipEntry pdfFile = zipFile.getEntry("junit-user-guide-5.9.3.pdf");
+            if (pdfFile != null) {
+                try (InputStream inputStreamPdf = zipFile.getInputStream(pdfFile)) {
+                    PDF pdf = new PDF(inputStreamPdf);
+                    Assertions.assertEquals("JUnit 5 User Guide", pdf.title);
+                }
+            } else {
+                throw new FileNotFoundException("PDF file not found");
+            }
+        }
+    }
 
     @Test
-    void testZip() throws Exception {
+    void testCsv() throws Exception {
         try (ZipFile zipFile = new ZipFile(new File("src/test/resources/zipFiles.zip"))) {
-
-            ZipEntry pdfFile = zipFile.getEntry("junit-user-guide-5.9.3.pdf");
             ZipEntry csvFile = zipFile.getEntry("pets.csv");
+            if (csvFile != null) {
+                try (InputStream inputStreamCsv = zipFile.getInputStream(csvFile);
+                     Reader reader = new InputStreamReader(inputStreamCsv)) {
+                    CSVReader csvReader = new CSVReader(reader);
+                    List<String[]> content = csvReader.readAll();
+
+                    final String[] getFirstRow = content.get(0);
+                    final String[] getSecondRow = content.get(1);
+                    final String[] getThirdRow = content.get(2);
+                    final String[] getFourthRow = content.get(3);
+
+                    Assertions.assertArrayEquals(new String[]{"Pet", "Pet's name", "Pet Age"}, getFirstRow);
+                    Assertions.assertArrayEquals(new String[]{"Cat", "Vasilii", "3"}, getSecondRow);
+                    Assertions.assertArrayEquals(new String[]{"Dog", "Evgen", "10"}, getThirdRow);
+                    Assertions.assertArrayEquals(new String[]{"Turtle", "Zhora", "99"}, getFourthRow);
+                }
+            } else {
+                throw new FileNotFoundException("CSV file not found");
+            }
+        }
+    }
+
+    @Test
+    void testXls() throws Exception {
+        try (ZipFile zipFile = new ZipFile(new File("src/test/resources/zipFiles.zip"))) {
             ZipEntry xlsFile = zipFile.getEntry("budget.xlsx");
+            if(xlsFile != null) {
+                try (InputStream inputStreamXls = zipFile.getInputStream(xlsFile)) {
+                    XLS xls = new XLS(inputStreamXls);
 
-            try (InputStream inputStreamPdf = zipFile.getInputStream(pdfFile)) {
-                PDF pdf = new PDF(inputStreamPdf);
-                Assertions.assertEquals("JUnit 5 User Guide", pdf.title);
-            }
-
-            try (InputStream inputStreamCsv = zipFile.getInputStream(csvFile);
-                 Reader reader = new InputStreamReader(inputStreamCsv)) {
-                CSVReader csvReader = new CSVReader(reader);
-                List<String[]> content = csvReader.readAll();
-
-                final String[] getFirstRow = content.get(0);
-                final String[] getSecondRow = content.get(1);
-                final String[] getThirdRow = content.get(2);
-                final String[] getFourthRow = content.get(3);
-
-                Assertions.assertArrayEquals(new String[]{"Pet", "Pet's name", "Pet Age"}, getFirstRow);
-                Assertions.assertArrayEquals(new String[]{"Cat", "Vasilii", "3"}, getSecondRow);
-                Assertions.assertArrayEquals(new String[]{"Dog", "Evgen", "10"}, getThirdRow);
-                Assertions.assertArrayEquals(new String[]{"Turtle", "Zhora", "99"}, getFourthRow);
-            }
-
-            try (InputStream inputStreamXls = zipFile.getInputStream(xlsFile)) {
-                XLS xls = new XLS(inputStreamXls);
-
-                Assertions.assertEquals("Остаток:", xls.excel.getSheetAt(0).getRow(1).getCell(4).getStringCellValue());
-                Assertions.assertEquals(50000, xls.excel.getSheetAt(0).getRow(2).getCell(4).getNumericCellValue());
+                    Assertions.assertEquals("Остаток:", xls.excel.getSheetAt(0).getRow(1).getCell(4).getStringCellValue());
+                    Assertions.assertEquals(50000, xls.excel.getSheetAt(0).getRow(2).getCell(4).getNumericCellValue());
+                }
+            } else {
+                throw new FileNotFoundException("XLS file not found");
             }
         }
     }
