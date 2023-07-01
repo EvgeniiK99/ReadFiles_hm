d3c057b0134869dcdd743306e07ea2eea5a7dc73
Evgenii Klimashin <evgeniiklimashin99@gmail.com>
2023-07-01 17:55:06 +0300

    Added assertj

--- src/test/java/tests/ZipFilesTests.java
+++ src/test/java/tests/ZipFilesTests.java
@@ -5,13 +5,14 @@ import com.codeborne.xlstest.XLS;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.opencsv.CSVReader;
 import model.GlossaryModel;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ZipFilesTests {
 
@@ -22,7 +23,7 @@ public class ZipFilesTests {
             if (pdfFile != null) {
                 try (InputStream inputStreamPdf = zipFile.getInputStream(pdfFile)) {
                     PDF pdf = new PDF(inputStreamPdf);
-                    Assertions.assertEquals("JUnit 5 User Guide", pdf.title);
+                    assertThat(pdf.title).isEqualTo("JUnit 5 User Guide");
                 }
             } else {
                 throw new FileNotFoundException("PDF file not found");
@@ -45,10 +46,10 @@ public class ZipFilesTests {
                     final String[] getThirdRow = content.get(2);
                     final String[] getFourthRow = content.get(3);
 
-                    Assertions.assertArrayEquals(new String[]{"Pet", "Pet's name", "Pet Age"}, getFirstRow);
-                    Assertions.assertArrayEquals(new String[]{"Cat", "Vasilii", "3"}, getSecondRow);
-                    Assertions.assertArrayEquals(new String[]{"Dog", "Evgen", "10"}, getThirdRow);
-                    Assertions.assertArrayEquals(new String[]{"Turtle", "Zhora", "99"}, getFourthRow);
+                    assertThat(getFirstRow).containsExactly("Pet", "Pet's name", "Pet Age");
+                    assertThat(getSecondRow).containsExactly("Cat", "Vasilii", "3");
+                    assertThat(getThirdRow).containsExactly("Dog", "Evgen", "10");
+                    assertThat(getFourthRow).containsExactly("Turtle", "Zhora", "99");
                 }
             } else {
                 throw new FileNotFoundException("CSV file not found");
@@ -64,8 +65,8 @@ public class ZipFilesTests {
                 try (InputStream inputStreamXls = zipFile.getInputStream(xlsFile)) {
                     XLS xls = new XLS(inputStreamXls);
 
-                    Assertions.assertEquals("Остаток:", xls.excel.getSheetAt(0).getRow(1).getCell(4).getStringCellValue());
-                    Assertions.assertEquals(50000, xls.excel.getSheetAt(0).getRow(2).getCell(4).getNumericCellValue());
+                    assertThat(xls.excel.getSheetAt(0).getRow(1).getCell(4).getStringCellValue()).isEqualTo("Остаток:");
+                    assertThat(xls.excel.getSheetAt(0).getRow(2).getCell(4).getNumericCellValue()).isEqualTo(50000);
                 }
             } else {
                 throw new FileNotFoundException("XLS file not found");
@@ -79,17 +80,17 @@ public class ZipFilesTests {
         File jsonFile = new File("src/test/resources/json.json");
         GlossaryModel glossaryModel = objectMapper.readValue(jsonFile, GlossaryModel.class);
 
-        Assertions.assertEquals("9999f9b9-c9b1-4f72-855b-a8a4bf15b1a9", glossaryModel.getProjectId());
-        Assertions.assertEquals("Test for QA.GURU", glossaryModel.getName());
-        Assertions.assertEquals(5499, glossaryModel.getCode());
+        assertThat(glossaryModel.getProjectId()).isEqualTo("9999f9b9-c9b1-4f72-855b-a8a4bf15b1a9");
+        assertThat(glossaryModel.getName()).isEqualTo("Test for QA.GURU");
+        assertThat(glossaryModel.getCode()).isEqualTo(5499);
 
-        Assertions.assertEquals("https://qa.guru/", glossaryModel.getLinks().get(0).getLink());
-        Assertions.assertEquals("https://github.com/EvgeniiK99", glossaryModel.getLinks().get(1).getLink());
+        assertThat(glossaryModel.getLinks().get(0).getLink()).isEqualTo("https://qa.guru/");
+        assertThat(glossaryModel.getLinks().get(1).getLink()).isEqualTo("https://github.com/EvgeniiK99");
 
-        Assertions.assertEquals("c45a9999", glossaryModel.getCreator().getId());
-        Assertions.assertEquals("Evgenii", glossaryModel.getCreator().getName());
-        Assertions.assertEquals("Klimashin", glossaryModel.getCreator().getSurname());
-        Assertions.assertTrue(glossaryModel.getCreator().getHuman());
+        assertThat(glossaryModel.getCreator().getId()).isEqualTo("c45a9999");
+        assertThat(glossaryModel.getCreator().getName()).isEqualTo("Evgenii");
+        assertThat(glossaryModel.getCreator().getSurname()).isEqualTo("Klimashin");
+        assertThat(glossaryModel.getCreator().getHuman()).isTrue();
     }
 
 }
